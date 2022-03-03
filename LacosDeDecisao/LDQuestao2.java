a765a176a9716cba10a648d296b9f57cffc90196
MatheusMSF <100284592+MatheusMSF@users.noreply.github.com>
2022-03-03 15:21:47 -0300

    Update LDQuestao2.java

--- LacosDeDecisao/LDQuestao2.java
+++ LacosDeDecisao/LDQuestao2.java
@@ -9,18 +9,18 @@ public class LDQuestao2 {
 		
 		Scanner ler = new Scanner(System.in);
 		
-		System.out.print("Digite o primeiro número: ");
+		System.out.print("Digite o primeiro nÃºmero: ");
 		n1 = ler.nextDouble();
 		
 		maior = n1;
 		
-		System.out.print("Digite o segundo número: ");
+		System.out.print("Digite o segundo nÃºmero: ");
 		n2 = ler.nextDouble();
 		
 		if(n1 > n2)menor = n2;
 		else {maior = n2; menor = n1;}
 		
-		System.out.print("Digite o terceiro número: ");
+		System.out.print("Digite o terceiro nÃºmero: ");
 		n3 = ler.nextDouble();
 		
 		if(n3 > maior) 
@@ -40,7 +40,7 @@ public class LDQuestao2 {
 		}
 		
 		System.out.print("\nOrdem crescente: " + menor 
-				+ ", " + meio + ", " + maior + "\nN1 " + n1 + "\nN2 " + n2 + "\nN3 " + n3);
+				+ ", " + meio + ", " + maior);
 	}
 
 }
